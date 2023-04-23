2347249af0315fcea31419b06ba2f5594ca9c232
code-death <poorvanshkavta938@gmail.com>
2023-04-23 22:21:26 +0530

    Completed with state working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import logo from  '../Logo.svg'
 import icon from '../metamask-icon.png'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <nav className='navbar'>
         <div className='left-section'>
-            <span className='logo-section'><img className='company-logo' src={logo} alt="company Logo" /><p className='company-name'>EXE Trade</p></span>
+            <Link to='/'><span className='logo-section'><img className='company-logo' src={logo} alt="company Logo" /><p className='company-name'>EXE Trade</p></span></Link>
             <ul className='nav-links'>
                 <Link to='/trade'><li className='nav-link'><span className='shadow'>Trade</span></li></Link>
                 <li className='nav-link'><span className='shadow'>About</span></li>
@@ -15,7 +15,7 @@ export default function Navbar() {
             </ul>
         </div>
         <div className='right-section'>
-            <button className='metamask-connect'><img className='metamask-logo' src={icon} alt="metamask logo" />Connect</button>
+            <button className='metamask-connect borderGreen'><img className='metamask-logo' src={icon} alt="metamask logo" />Connect</button>
         </div>
     </nav>
   )
--- src/components/Trade.js
+++ src/components/Trade.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import {useState} from 'react'
 import '../App.css'
 import Navbar from './Navbar'
-
 
 const positionData = [
     {
@@ -36,7 +35,19 @@ const balanceInfo = {
     usdc: '100'
 }
 
+
 export default function Trade() {
+
+    const [selected1, setSelected1] = useState(false)
+    const [selected2, setSelected2] = useState(true)
+
+    const handleSelected1 = () => {
+        setSelected1(value => !value)
+    }
+    const handleSelected2 = () => {
+        setSelected2(value => !value)
+    }
+
   return (
     <>
     <Navbar />
@@ -44,30 +55,30 @@ export default function Trade() {
         <div className='tradeBlock'>
             <h2 className='heading2'>Trade</h2>
             <div className='selectorBlock'>
-                <button className='selector selectorLeft'>
+                <button onClick={handleSelected1} className={'selector selectorLeft' + (selected1 ? ' selected' : '')}>
                     Buy EXE from USDC
                 </button>
-                <button className='selector selectorRight'>
+                <button onClick={handleSelected1} className={'selector selectorRight' + ((!selected1 ? ' selected' : ''))}>
                     Sell EXE for USDC
                 </button>
             </div>
             <div className='selectorBlock'>
-                <button className='selector selectorLeft'>
+                <button onClick={handleSelected2} className={'selector selectorLeft' + ((selected2 ? ' selected' : ''))}>
                     Maker (Limit)
                 </button>
-                <button className='selector selectorRight'>
+                <button onClick={handleSelected2} className={'selector selectorRight' + ((!selected2 ? ' selected' : ''))}>
                     Taker (Market)
                 </button>
             </div>
-            <p style ={{margin: '20px 0'}}>Current Price : $ 30.00</p>
-            <input type="text" name="amount" className='input inputAmount' placeholder='Enter Amount'/>
-            <input type="text" name="price" className='input inputPrice' placeholder='Enter Price'/>
-            <p style ={{margin: '20px 0'}}>Output : $ 30.00</p>
+            <p style ={{margin: '20px 0'}}>Current Price : <span style={{color: '#5fbf80', fontWeight: 600}}>$30.00</span> </p>
+            <input type="number" name="amount" className='input inputAmount' placeholder='Enter Amount' min='1'/>
+            {selected1 && <input type="number" name="price" className='input inputPrice' placeholder='Enter Price' min={'1'}/>}
+            <p style ={{margin: '20px 0'}}>Output : <span style={{color: '#5fbf80', fontWeight: 600}}>$30.00</span></p>
             <button className='metamask-connect heroButton'>Swap</button>
         </div>
-        <div className='tradeBlock'>
+        <div className='tradeBlock tradeBlock2'>
             <h2 className='heading2'>Your Positions</h2>
-                {positionData.map(item => (<div className='infoBlock'>
+                {positionData.map((item) => (<div key={item.key} className='infoBlock'>
                     <span className='infoItem'>{item.direction}</span>
                     <span className='infoItem'>Size: {item.size}</span>
                     <span className='infoItem'>Price: {item.price}</span>
@@ -77,7 +88,7 @@ export default function Trade() {
         <div className='tradeBlock3'>
         <div className='selectorBlock3'>
                 <h2 className='heading2'>
-                    Your Balnces
+                    Your Balances
                 </h2>
                 <h3 className='heading3'>EXE <span className='spacing'>{balanceInfo.exe}</span></h3>
                 <h3 className='heading3'>EXE Gain <span>{balanceInfo.nextexe}</span></h3>
